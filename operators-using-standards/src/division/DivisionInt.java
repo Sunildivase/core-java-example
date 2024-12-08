105c329e596b5d4b5cf27ebba70db39c56757f8c
Sunil <sunildivase07@gmail.com>
2024-12-08 13:25:11 +0530

    initial import

---
+++ operators-using-standards/src/division/DivisionInt.java
@@ -0,0 +1,19 @@
+package division;
+// pseudo code(steps):-
+// create two variables
+//perform division operation
+// create one variable for storing in memory
+// print output
+public class DivisionInt {
+    public void divNos(){
+        int a=20;
+        int b=5;
+        int c;
+        c=a/b;
+        System.out.println("Division: "+ c);
+    }
+    public static void main(String[] args){
+        DivisionInt demo=new DivisionInt();
+        demo.divNos();
+    }
+}
---
+++ operators-using-standards/src/subtraction/SubtractionInt.java
@@ -0,0 +1,20 @@
+package subtraction;
+// pseudo code (steps):-
+// create two variables
+// perform subtraction operation
+// create one variable for store
+// print the output
+
+public class SubtractionInt {
+    public void subNos(){
+        int a=13;
+        int b=34;
+        int c;
+        c=a-b;
+        System.out.println("Result: "+c);
+    }
+    public static void main(String[] args){
+        SubtractionInt y=new SubtractionInt();
+        y.subNos();
+    }
+}
