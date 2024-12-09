17c0307b9318d8c08f42b210c6d026c74affa099
Sunil <sunildivase07@gmail.com>
2024-12-09 15:06:49 +0530

    initial import

---
+++ conditional-statements-loops/src/dowhileloop/DoWhile.java
@@ -0,0 +1,21 @@
+package dowhileloop;
+
+public class DoWhile {
+
+    // in do while loop we give condition second
+
+    public static void main(String[] args) {
+
+        int no = 0;
+
+        do{
+            System.out.println("value "+no);
+            no++;
+
+        }
+        while(no <= 15);{
+
+            System.out.println("value "+ no);
+        }
+    }
+}
---
+++ conditional-statements-loops/src/whileloop/While.java
@@ -0,0 +1,26 @@
+package whileloop;
+
+//import java.util.Scanner;
+
+public class While {
+
+    // here we give condition first
+    // loops are used for iteration/repeatation
+
+    public static void main(String[] args) {
+
+//        Scanner scanner = new Scanner(System.in);
+
+//        System.out.println("pls enter value");
+//        int no = Integer.parseInt(scanner.nextLine());
+        int no = 0;
+
+        while(no <= 10){
+
+            System.out.println("iterated value :"+ no);
+            no++;
+        }
+
+
+    }
+}
