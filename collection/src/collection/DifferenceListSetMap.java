fb7576443471b9fade4bf45af7c79244c2dbeed9
Sunil <sunildivase07@gmail.com>
2025-01-01 16:14:02 +0530

    initial import

--- collection/src/collection/DifferenceListSetMap.java
+++ collection/src/collection/DifferenceListSetMap.java
@@ -1,11 +1,10 @@
 package collection;
 
-import org.w3c.dom.CDATASection;
-
 public class DifferenceListSetMap {
 
 //    List -  used for storing duplicate value
 //    Set - used for storing unique data
 //    Map - used for storing key value pair data
+
 }
 
--- collection/src/collection/MethodsOfList.java
+++ collection/src/collection/MethodsOfList.java
@@ -1,8 +1,6 @@
 package collection;
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
+import java.util.*;
 
 public class MethodsOfList {
 
@@ -63,11 +61,88 @@ public class MethodsOfList {
         newCityList.addAll(cities);
 
         newCityList.add("bengaluru");
+        newCityList.add("kolkata");
         System.out.println("new cities list: "+newCityList);
 
         System.out.println("-------------------------------------------------------------------------");
 
+       // contains() method - check the value inside list
+
+        if (newCityList.contains("navi mumbai")){
+            System.out.println("jaipur is inside the list");
+        }
+        else {
+            System.out.println("navi mumbai is not inside list");
+        }
+
+        System.out.println("------------------------------------------------------------------------");
+
+        // size() method - it returns length of lists
+
+        newCityList.add("jharkhand");
+        System.out.println("size of new city list: "+newCityList.size());
+        System.out.println("------------------------------------------------------------------------");
+
+       // iterator() method - iterator returns object that can be used to subsequently access element
+
+        Iterator<String > citiesIterator = newCityList.iterator();
+        while(citiesIterator.hasNext()){
+            System.out.println("next element in newCityList: "+citiesIterator.next());
+        }
+
+        System.out.println("-----------------------------------------------------------------------");
 
 
+        // hasnext - true and print data forward direction
+
+        // if we want backward direction data by using ListIterator
+
+        //listIterator() method - iterator returns object that can be used to subsequently access element
+
+
+        ListIterator<String> citiesListIterator = newCityList.listIterator();
+        while(citiesListIterator.hasNext()){
+            System.out.println("next element in newCityList using citiesListIterator :"+citiesListIterator.next());
+        }
+        System.out.println("-----------------------");
+        while (citiesListIterator.hasPrevious()){
+            System.out.println("previous element in newCityList using citiesListIterator :"+citiesListIterator.previous());
+        }
+
+        System.out.println("----------------------------------------------------------------------------------");
+
+        // for each example
+
+        System.out.println("using for each loop");
+        for(String city : newCityList){
+            System.out.println("next city :"+city);
+        }
+        System.out.println("-----------------------------------------------------------------------------");
+
+        //toArray() method - converts a list in array
+
+        String []citiesArray = newCityList.toArray(new String[newCityList.size()]);
+        for(String city : citiesArray){
+            System.out.println("conversion of array: "+city);
+        }
+        System.out.println("------------------------------------------------------------------------------");
+
+        //removeAll() method - remove all elements from list
+        System.out.println("before remove list: cities list "+cities);
+        System.out.println("before remove list : new cities list: "+ newCityList);
+        System.out.println("after remove all elements: "+newCityList.removeAll(cities));
+        System.out.println("remaining city list: "+newCityList);
+        System.out.println("---------------------------------------------------------------------------");
+
+        // clear() method - remove all elements from list more efficient than the removeAll()
+
+        System.out.println("size of new cities before clear: "+newCityList.size());
+        newCityList.clear();
+        System.out.println("size of city after clear: "+newCityList.size());
+
+        System.out.println("---------------------------------------------------------------------------");
+        
     }
+
+
 }
