e5b1b5afb65baeb94644620203b27707148a3571
Sunil <sunildivase07@gmail.com>
2025-01-01 13:03:14 +0530

    methods of list collection implement

---
+++ collection/src/collection/DifferenceListSetMap.java
@@ -0,0 +1,11 @@
+package collection;
+
+import org.w3c.dom.CDATASection;
+
+public class DifferenceListSetMap {
+
+//    List -  used for storing duplicate value
+//    Set - used for storing unique data
+//    Map - used for storing key value pair data
+}
+
