27b163e1dbc5cd3da32c686c77b2f12710cb85fc
Sunil <sunildivase07@gmail.com>
2025-02-11 16:48:57 +0530

    initial import

--- abstraction/src/Abstraction/AbstractionVsInterface.java
+++ abstraction/src/Abstraction/AbstractionVsInterface.java
@@ -4,12 +4,18 @@ public class AbstractionVsInterface {
 
 //    Abstraction:-
          //    - possibility of any method implementation
-         //     - can have abstract and non-abstract methods
-         //     -
+         //    - can have abstract and non-abstract methods
+         //    - can have static or non-static variables
+         //    - can have ny access modifiers.
+         //    - can have construction.
+         //    - can have single inheritance
 
 //    Interface:-
         //     - no any method implementation
         //     - only abstract methods
-
+        //     - can only have public static final constants
+        //     - only have public access modifiers.
+        //     - can not have constructions.
+        //     - can have multiple inheritance
 }
 
