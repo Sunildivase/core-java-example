cdfdc859a28fa22b11e06aeefc3eca59bf923b3c
Sunil <sunildivase07@gmail.com>
2025-02-27 17:25:58 +0530

    stream api implement

--- java-8-features/src/streamApi/StreamDemo.java
+++ java-8-features/src/streamApi/StreamDemo.java
@@ -1,7 +1,8 @@
 package streamApi;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
+import java.util.function.Function;
+import java.util.stream.Collectors;
 
 public class StreamDemo {
 
@@ -10,13 +11,44 @@ public class StreamDemo {
         StreamDemo streamDemo = new StreamDemo();
 
        List<Product> products = streamDemo.generateListData();
+
+
+
+    // without stream
+
+    List<Float> productPriceList = new ArrayList<>();
+
+   for(Product product: products)
+        // filtering data of list
+        if(product.price()>35999){
+            productPriceList.add(product.price()); // adding price of products to list
+        }
+
+          System.out.println("list without streams: "+productPriceList);
+
+        // using streams
+        //  we will optimize code using stream
+        List<Float> productPriceList2 = products.stream() //stream is print the data
+                .parallel() // parallel processing
+                .filter(product -> product.price()>35999 && product.price()<60999) //filtering data(instead of if-else)
+                .map(product -> product.price()) // fetching price
+                .toList(); // collecting as list
+        // .collect(Collectors.toList()) // collecting as list
+
+        System.out.println("product price list with stream: "+productPriceList2);
+
+        //-----------------------------------------------------------------------------
+
+//        Map<Integer , Product> productMap = streamDemo.generateListData();
+
     }
+
 
     // externalized data generation.
     private List<Product> generateListData(){
 
         List<Product> products = new ArrayList<>();
-        
+
         // adding products
 //        products.add(new Product(1,"Hp laptop",25000f));
 //        products.add(new Product(2,"sony laptop",39000f));
@@ -30,3 +62,16 @@ public class StreamDemo {
                 (new Product(4,"asus laptop",41999f)));
     }
 }
+
+//externalized data generation
+
+//private Map<Integer ,Product> generateMapData(){
+//    //.collect() & Collectors.toMap() demonstrate
+//    //Function.identity()
+//
+//return generateListData().stream();
+////.collect(Collectors.toMap(product::id , product -> product))
+//    .collect(Collectors.toMap(Product::id, Function.identity()));
+//}
+
+
