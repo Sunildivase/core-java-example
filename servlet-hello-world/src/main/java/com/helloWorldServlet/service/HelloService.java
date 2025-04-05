214744b1b1fcdda0ebdd283f58b8402aa8024916
Sunil <sunildivase07@gmail.com>
2025-04-05 12:18:26 +0530

    new repo created

--- servlet-hello-world/src/main/java/com/helloWorldServlet/service/HelloService.java
+++
@@ -1,8 +0,0 @@
-package com.helloWorldServlet.service;
-
-public class HelloService {
-
-    public String greet(String name) {
-        return "hello " + (name.isBlank() ? "world!!" : name);
-    }
-}
